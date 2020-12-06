05d86e1afc92053ef8164016f9ceb77f9061ac59
Williams Rivas <williams.rivas1@gmail.com>
2020-12-06 01:46:38 -0300

    add HomeController unit test

---
+++ src/test/java/msvcdojo/mysvc/HomeControllerTest.java
@@ -0,0 +1,34 @@
+package msvcdojo.mysvc;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = MockServletContext.class)
+@WebAppConfiguration
+public class HomeControllerTest {
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
+    }
+}
